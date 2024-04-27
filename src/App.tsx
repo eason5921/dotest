113a05753075a1d0ec6a63a8a582af79c43fe8f8
eason5921 <1255026858@qq.com>
2024-04-27 23:17:20 +0800

    10秒代码变更检查测试2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Typewriter from "./components/Typewriter";
 
 function App() {
+    useEffect(()=>{
+        fetch('http://www.dengpu-dev.site:3002/')
+    },[])
   return (
     <div className="App">
         腾讯云推送测试88888
